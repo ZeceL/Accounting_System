4dfc11ea9fcefb6a125e7315768cb174a1ac7731
ZeceL <aleksejcernysev68@gmail.com>
2024-04-21 18:53:54 +0300

    Добавил форму входа и формы для работы с базой данных.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(303, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(205, 29);
+            label1.TabIndex = 0;
+            label1.Text = "Вход в систему";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ControlText;
+            label2.Location = new Point(242, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 29);
+            label2.TabIndex = 1;
+            label2.Text = "Логин";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ControlText;
+            label3.Location = new Point(247, 214);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 29);
+            label3.TabIndex = 2;
+            label3.Text = "Пароль";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(247, 176);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(250, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(247, 255);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(250, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(338, 299);
+            button1.Name = "button1";
+            button1.Size = new Size(99, 39);
+            button1.TabIndex = 5;
+            button1.Text = "Войти";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
